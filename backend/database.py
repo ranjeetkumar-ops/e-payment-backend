bcf17411b2df3785a5e70faed567cf7d48591e25
ranjeetkumar-ops <ranjeet.kumar@cjdarcl.com>
2026-04-04 00:18:43 +0530

    fix: render port config

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,48 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/e_payment_system"
+# DATABASE_URL = "postgresql://e_payment_system_user:jfZSfISYaNMndK5V6rKr1QIooZpiRlea@dpg-d77s3o94tr6s73d64lrg-a.ohio-postgres.render.com/e_payment_system"
 
-engine = create_engine(DATABASE_URL, echo=True)
+# engine = create_engine(DATABASE_URL, echo=True)
+
+# SessionLocal = sessionmaker(
+# autocommit=False,
+# autoflush=False,
+# bind=engine
+# )
+
+# Base = declarative_base()
+
+# # ⭐ THIS FUNCTION IS REQUIRED
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+load_dotenv()  # Load environment variables from .envfile      
+DATABASE_URL = "postgresql://e_payment_system_user:jfZSfISYaNMndK5V6rKr1QIooZpiRlea@dpg-d77s3o94tr6s73d64lrg-a.ohio-postgres.render.com/e_payment_system"
+# Fix for Render (important)
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True  # ⭐ prevents connection timeout issues
+)
 
 SessionLocal = sessionmaker(
-autocommit=False,
-autoflush=False,
-bind=engine
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
 Base = declarative_base()
 
-# ⭐ THIS FUNCTION IS REQUIRED
 def get_db():
     db = SessionLocal()
     try:
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:"https://e-payment-backend-2rwj.onrender.com"
+    //  baseURL:"http://127.0.0.1:8000"
+  
 })
 
 instance.interceptors.request.use((config)=>{
