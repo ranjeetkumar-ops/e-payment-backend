f11144d545470753dbc4d58e0b866a956fef2cec
ranjeetkumar-ops <ranjeet.kumar@cjdarcl.com>
2026-04-03 14:51:40 +0530

    fix render start issue + cors

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://prps-cjdarcl.web.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
